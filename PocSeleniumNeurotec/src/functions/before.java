18e891fc82321dc81cd89a426f7aea1594edc443
guto_ <guto_@DESKTOP-5L2G1IR>
2022-05-13 15:29:30 -0300

    adicionando diretorios com o webdrive de forma correta

--- PocSeleniumNeurotec/src/functions/before.java
+++ PocSeleniumNeurotec/src/functions/before.java
@@ -17,49 +17,53 @@ public class before {
 	public static WebDriver driver;
 	public static String dataFormatada;
 	public static String diretorio;
-	
-	
+
+	/*
+	 * esta classe test() ira ser executada antes de todos os testes independente do script criado
+	 * aqui é criado a pasta de execução e gerado arquivo de log
+	 */
+
 	@BeforeClass
 	public static void test() throws Exception  {
 		ChromeOptions opt = new ChromeOptions();
 		opt.addArguments("--disable-notifications");
-		
-		
-		System.setProperty("webdriver.chrome.driver", "C:\\chromedriver\\chromedriver.exe");
+
+
+		System.setProperty("webdriver.chrome.driver", "webdrivers\\ChromeDriver\\100\\chromedriver.exe");
 		driver = new ChromeDriver(opt);
 		//evidenciasPrint.criaDiretorio();
-		
+
 		Date data = new Date();
 		SimpleDateFormat dataFormat = new SimpleDateFormat("yyyy-MM-dd-HHmmss");
-		
+
 		dataFormatada = dataFormat.format(data);
-		diretorio = ("C:\\workspace\\PocSeleniumNeurotec\\Evidencias\\" + dataFormatada);
+		diretorio = ("PocSeleniumNeurotec\\Evidencias\\" + dataFormatada);
 		new File(diretorio).mkdir();
 		geradorLog("Inicio da execuçao","INFO");
-		
+
 
 	}
 	public static String geraLog;
-	
+
 	public static void geradorLog(String logInfo, String lvl) throws Exception{
 		//arquivo de log
 		try {
-			
-		FileWriter arq = new FileWriter(diretorio + "\\"+ dataFormatada + ".txt", true);
-		BufferedWriter writer = new BufferedWriter(arq);
-		
-	
-		writer.write("[" + dataFormatada + "]"+"[" + lvl + "] - "+  logInfo + "\n");
-		writer.flush();
-		writer.close();
-		
+
+			FileWriter arq = new FileWriter(diretorio + "\\"+ dataFormatada + ".txt", true);
+			BufferedWriter writer = new BufferedWriter(arq);
+
+
+			writer.write("[" + dataFormatada + "]"+"[" + lvl + "] - "+  logInfo + "\n");
+			writer.flush();
+			writer.close();
+
 		}catch(Exception e) {
 			e.printStackTrace();
 		}
-		
-		
+
+
 	}
-	
+
 }
 
 
--- PocSeleniumNeurotec/src/scripts/pocNeurotecKabum.java
+++ PocSeleniumNeurotec/src/scripts/pocNeurotecKabum.java
@@ -5,7 +5,24 @@ import functions.*;
 
 public class pocNeurotecKabum {
 
-	//Funcao para acessar a home page do site.
+
+	/*
+	 * Script de teste utilizando Selenium e java.
+	 * Acessar o site www.kabum.com
+	 * realizar uma busca de item
+	 * selecionar um item
+	 * digitar o cep e coletar informações de frete
+	 * fechar pop up com as datas de frete
+	 * clicar em comprar
+	 * verificar se o protudo esta na cesta
+	 * 
+	 * OBS: os passos 7 e 8 estao comentados pois a garantia nao aparece após clicar em comprar 
+	 * e nao existe um botao de continuar, estes passos foram ignorados pois é um cenário de sucesso e nao de erro
+	 * 
+	 * 
+	 * Código criado por Gutenberg de Souza Barros
+	 * 
+	 */
 
 	@Test
 	public void pocNeurotecKabumSelenium() throws Exception{
@@ -13,7 +30,7 @@ public class pocNeurotecKabum {
 		before.test();
 
 		//tests
-		
+
 		homePage.test();						//1- Acessar o site www.americanas.com
 		busca_item.test();						//2- Buscar por "console ps4"
 		selecionaItem.test();					//3- Selecionar o Primeiro console
